9bcdd888c4e4da4c44c5882d81cb270711df91f7
bageljr <aryanagarwal@my.unt.edu>
2021-03-12 14:23:47 -0600

    GUI modularization pt. 1

--- gui.py
+++ gui.py
@@ -11,15 +11,14 @@ from scipy.optimize import curve_fit
 from scipy.interpolate import interp1d
 from tqdm.auto import trange
 import matplotlib.pyplot as plt
-import RASHG_functions as RASHG
 import xarray as xr
 import holoviews as hv
 pn.extension('plotly')
 hv.extension('bokeh', 'plotly')
 if(Machine):
-    import instruments
+    from instruments import instruments
 else:
-    print("RUNNING IN MACHINELESS MODE, data is literally random  or zero")
+    from random_instruments import instruments
 class grapher(param.Parameterized):
     colorMap = param.ObjectSelector(default="fire", objects=hv.plotting.util.list_cmaps())
     cPol = param.Number(default=0)
@@ -39,15 +38,12 @@ class grapher(param.Parameterized):
         self.polbar = tqdm(desc = "polarization") #polarization
         self.init_vars()
         self.cache = np.random.rand(100,100)
-        if(Machine):
-            self.init_instruments()
+        self.instruments=instruments(self.x1,self.x2,self.y1,self.y2)
     def init_vars(self):
-        if(not Machine):
-            self.x1 = 0
-            self.x2 = 100
-            self.y1 = 0
-            self.y2 = 100 #todo make GUI
-        escape_delay = 0
+        self.x1 = 0
+        self.x2 = 100
+        self.y1 = 0
+        self.y2 = 100 #todo make GUI
         x = self.x2 - self.x1
         y = self.y2 - self.y1
         polarization = 180
@@ -59,7 +55,6 @@ class grapher(param.Parameterized):
         wavend = 800
         wavstep = 2
         self.wavwait = 5 #value is in seconds
-        exp_time = 1000
         self.data = netCDF4.Dataset(self.filename, 'w')
         x = self.data.createDimension('x', x)
         y = self.data.createDimension('y', y)
@@ -107,28 +102,6 @@ class grapher(param.Parameterized):
         self.pwr[:] = np.arange(pow_start,pow_stop,pow_step, dtype=np.uint16)
         self.wav[:] = np.arange(wavstart, wavend, wavstep, dtype=np.uint16)
         self.cache=np.random.rand(x.size,y.size)
-    def init_instruments(self):
-        self.cam, A, B, C = RASHG.InitializeInstruments()
-        self.rbot = A
-        self.rtop = B
-        self.atten = C
-        rm = pyvisa.ResourceManager()
-        self.Pmeter = rm.open_resource('ASRL3::INSTR')
-        self.MaiTai = rm.open_resource('ASRL1::INSTR')
-        self.MaiTai.write("SHUT 0") #close the shutter
-        print('Shutter closed')
-        self.rbot.home(wait=False)
-        self.rtop.home(wait=False)
-        print('Homing stages')
-        self.atten.home(wait=True)
-        self.cam.roi = (x1, x2, y1, y2) #x1, x2, y1, y2
-        PC, PCcov, WavPowAng, pc = RASHG.PCFit(calibration_file)
-
-    #main collection loop; parameters should be set here before running the script. Comment out and untab inner loops as needed.
-        atten.move_to(0*u.degree, wait=True)
-        time.sleep(escape_delay)
-        MaiTai.write("SHUT 1")
-        print('Shutter opened')
     def gather_data(self, event = None):
         self.button.disabled = True
         self.live = False
@@ -139,32 +112,16 @@ class grapher(param.Parameterized):
         self.wbar.reset(total=len(wit))
         oit = range(self.ori.size)
         for w in wit:
-            if(Machine):
-                self.MaiTai.write(f"WAV {wav[w]}")
-                time.sleep(self.wavwait) #takes value in seconds
+            self.instruments.wav_step()
             self.pbar.reset(total=len(pit))
             for pw in pit:
-                if (Machine):
-                    self.atten_pos = RASHG.InvSinSqr(pw, *PC[int((wav[w]-780)/2)])
-                    self.atten.move_to(atten_pos, wait=True)
+                self.instruments.power_step()
                 self.obar.reset(total=len(oit))
                 for o in oit:
                     self.polbar.reset(total=len(polit))
                     for p in polit:
                         self.cPol = p
-                        if(Machine):
-                            if o == o[1]:
-                                sys_offset = 45
-                            pos = p * u.degree
-                            pos_top = pos - rtop.offset + sys_offset
-                            pos_bot = pos - rbot.offset
-                            rtop.move_to(pos_top, wait=False)
-                            rbot.move_to(pos_bot, wait=True)
-                            self.cache = cam.get_frame(exp_time=exp_time)
-                            shg[:,:,o,p,pw,w] = self.cache
-                        else:
-                            self.cache=np.random.rand(self.x.size,self.y.size)
-                            self.shg[:,:,o,p,pw,w] = self.cache
+                        shg[:,:,o,p,pw,w] = self.instruments.get_frame(o,p)
                         self.polbar.update()
                     self.obar.update()
                 self.pbar.update()
@@ -173,10 +130,7 @@ class grapher(param.Parameterized):
         print("Initializing live view") 
         self.button2.disabled = True
         while self.live:
-            if(Machine):
-                self.cache = cam.get_frame(exp_time=exp_time)
-            else:
-                self.cache = np.zeros((self.x.size,self.y.size))
+            self.cache = self.live()
             self.cPol = self.cPol + 1
             sleep(self.refresh)
             if not live:
