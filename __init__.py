2654a4d2a4e79cd3762e53f028341dd47f5992d2
bageljr <aryanagarwal@my.unt.edu>
2021-06-02 11:54:12 -0500

    Dynamic module import

--- __init__.py
+++ __init__.py
@@ -1,24 +1,19 @@
 import os
+from inspect import isclass
+from pkgutil import iter_modules
+from pathlib import Path
+from importlib import import_module
 
 instruments = {}
-for file in os.listdir('.'):
-    print(file)
-try:
-    from . import instruments_random as random
-except ImportError:
-    print("random import failed")
-else:
-    instruments["random"] = random
-try:
-    from . import instruments_RASHG as RASHG
-except ImportError:
-    print("RASHG import failed")
-else:
-    instruments["RASHG"] = RASHG
-try:
-    from . import instruments_scanimage
-except ImportError:
-    print("scanimage import failed")
-else:
-    instruments["scanimage"] = instruments_scanimage
+package_dir = str(Path(__file__).resolve().parent)
+for (_, module_name, _) in iter_modules([package_dir]):
+    if str(module_name).__contains__("instruments"):
+        sname = module_name.replace("instruments_", "")
+        if not str(sname).__contains__("base"):
+            try:
+                module = import_module(f"{__name__}.{module_name}")
+            except ImportError:
+                print(f"{sname} import failed")
+            else:
+                instruments[str(sname)] = module
 from . import gui
